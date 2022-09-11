d7a971f7e6f2bc23747be03bdb2d01ed2ec3d5f7
oferisak <doctoroots@gmail.com>
2022-09-11 09:22:57 +0300

    updated the script to change the exon number in minus strand transcripts

--- apps/gene_list_coverage_app/gene_list_coverage_app.R
+++ apps/gene_list_coverage_app/gene_list_coverage_app.R
@@ -16,6 +16,7 @@ library(ggsci)
 refseq_to_gene<-readr::read_delim('./accessory_data/refseq_to_gene.txt')
 # remove bad chr
 refseq_to_gene<-refseq_to_gene%>%filter(!grepl('_',chrom))
+all_gene_symbols<-refseq_to_gene%>%pull(gene_symbol)%>%unique()
 
 # fix gene names to be the approved ones
 standard_genenames_table<-readr::read_delim('./accessory_data/standard_genenames_db_2022-04-07.csv')
@@ -33,9 +34,15 @@ refseq_curated<-readr::read_delim('./accessory_data/refseq_hg19_curated_exons_20
     filter(!grepl('_',chr))%>%
     mutate(transcript_name=stringr::str_extract(interval_name,'(.+)_(exon|cds)')%>%stringr::str_replace('_(exon|cds)',''),
            exon_num=as.numeric(stringr::str_extract(interval_name,'(exon|cds)_\\d+')%>%stringr::str_extract('\\d+')))
+
 # add total number of exons
 refseq_curated<-refseq_curated%>%left_join(refseq_curated%>%group_by(transcript_name,chr)%>%summarize(total_exons=n()))
+
 refseq_with_segdup<-refseq_curated%>%left_join(segdup_intersect_data%>%select(-c(start,end)),by=c('transcript_name','chr','exon_num'))
+
+# fix exon number (in minus strand transcripts it should be the other way around..)
+# the minus one is because the exons start at 0
+refseq_with_segdup<-refseq_with_segdup%>%mutate(exon_num=ifelse(strand=='-',total_exons-exon_num-1,exon_num))
 
 # Define UI ####
 ui <- dashboardPage(dashboardHeader(title = sprintf('Disclaimer Generator'),
@@ -98,6 +105,7 @@ ui <- dashboardPage(dashboardHeader(title = sprintf('Disclaimer Generator'),
                                         fileInput("gene_list_file", "Select gene list file",
                                                   multiple = FALSE,
                                                   accept = c(".csv",".tsv",'.xls','.xlsx')),
+                                        selectizeInput('gene_list_selection',choices = NULL,selected=NULL,multiple=TRUE,label='Gene List'),
                                         div(DT::dataTableOutput(outputId = 'gene_list_table'), 
                                             style='font-size:125%;'))
                                     ),
@@ -131,15 +139,9 @@ ui <- dashboardPage(dashboardHeader(title = sprintf('Disclaimer Generator'),
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    # per transcript ####
-    observeEvent(input$gene_list_file,{
-        if (grepl('xls',input$gene_list_file$datapath)){
-            gene_list<-readxl::read_excel(input$gene_list_file$datapath,col_names = F)
-            colnames(gene_list)[1]<-'gene_symbol'
-        }else{
-            gene_list<-readr::read_delim(input$gene_list_file$datapath,col_names = c('gene_symbol'))
-        }
-        
+    updateSelectizeInput(inputId = 'gene_list_selection', choices = all_gene_symbols, server = TRUE)
+    
+    update_gene_list<-function(gene_list){
         # fix gene names
         # first check if any of the provided gene symbols is not found in the refseq gene name table
         gene_list_genes_not_in_refseq<-gene_list%>%
@@ -154,20 +156,14 @@ server <- function(input, output) {
             mutate(gene_symbol=ifelse(gene_symbol%in%gene_list_genes_not_in_refseq$provided_gene_symbol,
                                       gene_list_genes_not_in_refseq%>%filter(provided_gene_symbol==gene_symbol)%>%slice(1)%>%pull(approved_gene_symbol),#if there are more than one approved gene symbol for the previous symbol, pick the first one.
                                       gene_symbol))
-        
-        #gene_list<-gene_list%>%mutate(gene_found_in_reference=gene_symbol%in%refseq_to_gene$gene_name)
-        gene_list_annotated<-gene_list%>%select(gene_symbol,gene_name_provided)%>%left_join(refseq_to_gene)
-        #fix input gene names to match refseq
-        gene_symbols<- gene_list%>%pull(gene_symbol)
-        output$gene_list_table<-DT::renderDataTable(gene_list_annotated,
-                                                    options=list(scrollX=F,pageLength=10),
-                                                    selection='single',
-                                                    rownames= FALSE,
-                                                    filter = list(position = 'top', clear = FALSE))
-        # add transcript names
+        return(gene_list)
+    }
+    
+    update_seg_dup<-function(gene_list_annotated){
         # join gene list with segdup
         genes_seg_data<-gene_list_annotated%>%
-            inner_join(segdup_intersect_data)
+            #inner_join(segdup_intersect_data)
+            inner_join(refseq_with_segdup)
         
         observeEvent(input$lib_prep_kit_plots,{
             observeEvent(input$segdup_plots_thresh,{
@@ -196,59 +192,192 @@ server <- function(input, output) {
                     
                     box(title = gene_name_to_plot,renderPlot(cov_plot))
                 }
-
+                
                 output$per_transcript_cov_plot<-renderUI({
                     lapply(to_plot%>%arrange(gene_symbol)%>%pull(gene_symbol)%>%unique(), plot_coverage_data)
                 })
             })
-
+            
             #output$per_transcript_cov_plot<-renderPlotly(cov_plot)
-
+            
         })
+        return(genes_seg_data)
+    }
+    
+    update_disclaimer_table<-function(gene_list_annotated,genes_seg_data){
+        observeEvent(input$lib_prep_kit_disclaimer_table,{
+            observeEvent(input$disclaimer_segdup_thresh,{
+                disclaimer_segdup_thresh<-input$disclaimer_segdup_thresh
+                high_dup_exons<-genes_seg_data%>%
+                    filter(dup_perc>disclaimer_segdup_thresh)%>%
+                    group_by(gene_symbol,transcript_name)%>%
+                    summarize(exons_with_dup_level_above_thresh=paste0(unique(exon_num+1),collapse=', '))
+                
+                disclaimer_table<-gene_list_annotated%>%
+                    left_join(refseq_curated%>%select(transcript_name,total_exons)%>%distinct())%>%
+                    left_join(high_dup_exons)%>%
+                    mutate(in_disclaimer=factor(ifelse(!(is.na(exons_with_dup_level_above_thresh)),T,F)))
+                
+                if (input$lib_prep_kit_disclaimer_table=='select'){
+                    disclaimer_table<-disclaimer_table%>%filter(cannonical)
+                }
+                
+                print(disclaimer_table)
+                disclaimer_text<-disclaimer_table%>%filter(in_disclaimer==TRUE)%>%
+                    mutate(as_text=glue('{gene_symbol}:{transcript_name}'))%>%
+                    mutate(as_text=ifelse(!is.na(exons_with_dup_level_above_thresh),glue('{as_text}:Exons with >10% with >{disclaimer_segdup_thresh*100}% homology:{exons_with_dup_level_above_thresh}'),as_text))
+                
+                dup_column_name<-glue('Exons with >10% with >{disclaimer_segdup_thresh*100}% homology')
+                disclaimer_table<-disclaimer_table%>%
+                    rename('Add to disclaimer'=in_disclaimer,
+                           !!sym(dup_column_name):=exons_with_dup_level_above_thresh,
+                           'Gene name'=gene_symbol,
+                           'Transcript name'=transcript_name)
+                output$disclaimer_table<-DT::renderDataTable(DT::datatable(disclaimer_table,
+                                                                           options=list(scrollX=F,pageLength=nrow(disclaimer_table),
+                                                                                        buttons = c('csv', 'excel'),
+                                                                                        dom = 'Bfrtip'),
+                                                                           extensions = 'Buttons',
+                                                                           selection='single',
+                                                                           rownames= FALSE,
+                                                                           filter = list(position = 'top', clear = FALSE)))
+                
+                output$disclaimer_text<-renderText(paste0(disclaimer_text%>%pull(as_text),collapse=' | '))
+                
+            })
+        })
+    }
+    
+    gene_list<-NULL
+    gene_list_annotated<-NULL
+    observeEvent(input$gene_list_selection,{
+        added_genes<-setdiff(input$gene_list_selection,gene_list$gene_symbol)
+        gene_list<-gene_list%>%bind_rows(data.frame(gene_symbol=added_genes,gene_name_provided=added_genes))
+        
+        gene_list_annotated<-gene_list%>%select(gene_symbol,gene_name_provided)%>%left_join(refseq_to_gene)
+        #fix input gene names to match refseq
+        gene_symbols<- gene_list%>%pull(gene_symbol)
+        
+        output$gene_list_table<-DT::renderDataTable(gene_list_annotated,
+                                                    options=list(scrollX=F,pageLength=10),
+                                                    selection='single',
+                                                    rownames= FALSE,
+                                                    filter = list(position = 'top', clear = FALSE))
+        genes_seg_data<-update_seg_dup(gene_list_annotated)
+        update_disclaimer_table(gene_list_annotated,genes_seg_data)
+    })
+    # per transcript ####
+    observeEvent(input$gene_list_file,{
+        if (grepl('xls',input$gene_list_file$datapath)){
+            gene_list<-readxl::read_excel(input$gene_list_file$datapath,col_names = F)
+            colnames(gene_list)[1]<-'gene_symbol'
+        }else{
+            gene_list<-readr::read_delim(input$gene_list_file$datapath,col_names = c('gene_symbol'))
+        }
+        
+        gene_list<-update_gene_list(gene_list)
+
+        updateSelectizeInput(inputId = 'gene_list_selection', choices = all_gene_symbols,selected=gene_list$gene_symbol, server = TRUE)
+        
+        #gene_list<-gene_list%>%mutate(gene_found_in_reference=gene_symbol%in%refseq_to_gene$gene_name)
+        
+        gene_list_annotated<-gene_list%>%select(gene_symbol,gene_name_provided)%>%left_join(refseq_to_gene)
+        #fix input gene names to match refseq
+        gene_symbols<- gene_list%>%pull(gene_symbol)
+        
+        
+        output$gene_list_table<-DT::renderDataTable(gene_list_annotated,
+                                                    options=list(scrollX=F,pageLength=10),
+                                                    selection='single',
+                                                    rownames= FALSE,
+                                                    filter = list(position = 'top', clear = FALSE))
+        # add transcript names
+        genes_seg_data<-update_seg_dup(gene_list_annotated)
+        update_disclaimer_table(gene_list_annotated,genes_seg_data)
+        # # join gene list with segdup
+        # genes_seg_data<-gene_list_annotated%>%
+        #     inner_join(segdup_intersect_data)
+        # 
+        # observeEvent(input$lib_prep_kit_plots,{
+        #     observeEvent(input$segdup_plots_thresh,{
+        #         
+        #         segdup_thresh<-input$segdup_plots_thresh
+        #         transcripts_with_dups<-refseq_with_segdup%>%filter(dup_perc>segdup_thresh)%>%pull(transcript_name)%>%unique()
+        #         to_plot<-refseq_with_segdup%>%
+        #             mutate(dup_level=cut(dup_perc,breaks=c(0,segdup_thresh,1),labels=c(glue('<{segdup_thresh}'),glue('>{segdup_thresh}'))))%>%
+        #             mutate(dup_level=factor(ifelse(is.na(dup_level),glue('<{segdup_thresh}'),as.character(dup_level))))%>%
+        #             filter(transcript_name %in% transcripts_with_dups)%>%
+        #             left_join(refseq_to_gene)%>%
+        #             filter(transcript_name%in%(gene_list_annotated$transcript_name))
+        #         if (input$lib_prep_kit_plots=='select'){
+        #             to_plot<-to_plot%>%filter(cannonical)
+        #         }
+        #         
+        #         plot_coverage_data<-function(gene_name_to_plot){
+        #             cov_plot<-to_plot%>%filter(gene_symbol==gene_name_to_plot)%>%
+        #                 ggplot(aes(x=transcript_name,xend=transcript_name,y=start,yend=end,col=dup_level))+
+        #                 geom_segment(size=10)+
+        #                 coord_flip()+
+        #                 scale_color_manual(values=c('gray','darkred'),drop=F)+
+        #                 theme_minimal()+
+        #                 labs(x=NULL,y=NULL,color='Duplication level')+
+        #                 theme(legend.position = 'bottom')
+        #             
+        #             box(title = gene_name_to_plot,renderPlot(cov_plot))
+        #         }
+        # 
+        #         output$per_transcript_cov_plot<-renderUI({
+        #             lapply(to_plot%>%arrange(gene_symbol)%>%pull(gene_symbol)%>%unique(), plot_coverage_data)
+        #         })
+        #     })
+        # 
+        #     #output$per_transcript_cov_plot<-renderPlotly(cov_plot)
+        # 
+        # })
         
         # Disclaimer table tab
         
-        observeEvent(input$lib_prep_kit_disclaimer_table,{
-          observeEvent(input$disclaimer_segdup_thresh,{
-            disclaimer_segdup_thresh<-input$disclaimer_segdup_thresh
-            high_dup_exons<-genes_seg_data%>%
-                filter(dup_perc>disclaimer_segdup_thresh)%>%
-                group_by(gene_symbol,transcript_name)%>%
-                summarize(exons_with_dup_level_above_thresh=paste0(unique(exon_num+1),collapse=', '))
-            
-            disclaimer_table<-gene_list_annotated%>%
-                left_join(refseq_curated%>%select(transcript_name,total_exons)%>%distinct())%>%
-                left_join(high_dup_exons)%>%
-                mutate(in_disclaimer=factor(ifelse(!(is.na(exons_with_dup_level_above_thresh)),T,F)))
-            
-            if (input$lib_prep_kit_disclaimer_table=='select'){
-                disclaimer_table<-disclaimer_table%>%filter(cannonical)
-            }
-            
-            print(disclaimer_table)
-            disclaimer_text<-disclaimer_table%>%filter(in_disclaimer==TRUE)%>%
-                mutate(as_text=glue('{gene_symbol}:{transcript_name}'))%>%
-                mutate(as_text=ifelse(!is.na(exons_with_dup_level_above_thresh),glue('{as_text}:Exons with >10% with >{disclaimer_segdup_thresh*100}% homology:{exons_with_dup_level_above_thresh}'),as_text))
-            
-            dup_column_name<-glue('Exons with >10% with >{disclaimer_segdup_thresh*100}% homology')
-            disclaimer_table<-disclaimer_table%>%
-                rename('Add to disclaimer'=in_disclaimer,
-                       !!sym(dup_column_name):=exons_with_dup_level_above_thresh,
-                       'Gene name'=gene_symbol,
-                       'Transcript name'=transcript_name)
-            output$disclaimer_table<-DT::renderDataTable(DT::datatable(disclaimer_table,
-                                                     options=list(scrollX=F,pageLength=nrow(disclaimer_table),
-                                                                  buttons = c('csv', 'excel'),
-                                                                  dom = 'Bfrtip'),
-                                                     extensions = 'Buttons',
-                                                     selection='single',
-                                                     rownames= FALSE,
-                                                     filter = list(position = 'top', clear = FALSE)))
-                
-            output$disclaimer_text<-renderText(paste0(disclaimer_text%>%pull(as_text),collapse=' | '))
-            
-          })
-        })
+        # observeEvent(input$lib_prep_kit_disclaimer_table,{
+        #   observeEvent(input$disclaimer_segdup_thresh,{
+        #     disclaimer_segdup_thresh<-input$disclaimer_segdup_thresh
+        #     high_dup_exons<-genes_seg_data%>%
+        #         filter(dup_perc>disclaimer_segdup_thresh)%>%
+        #         group_by(gene_symbol,transcript_name)%>%
+        #         summarize(exons_with_dup_level_above_thresh=paste0(unique(exon_num+1),collapse=', '))
+        #     
+        #     disclaimer_table<-gene_list_annotated%>%
+        #         left_join(refseq_curated%>%select(transcript_name,total_exons)%>%distinct())%>%
+        #         left_join(high_dup_exons)%>%
+        #         mutate(in_disclaimer=factor(ifelse(!(is.na(exons_with_dup_level_above_thresh)),T,F)))
+        #     
+        #     if (input$lib_prep_kit_disclaimer_table=='select'){
+        #         disclaimer_table<-disclaimer_table%>%filter(cannonical)
+        #     }
+        #     
+        #     print(disclaimer_table)
+        #     disclaimer_text<-disclaimer_table%>%filter(in_disclaimer==TRUE)%>%
+        #         mutate(as_text=glue('{gene_symbol}:{transcript_name}'))%>%
+        #         mutate(as_text=ifelse(!is.na(exons_with_dup_level_above_thresh),glue('{as_text}:Exons with >10% with >{disclaimer_segdup_thresh*100}% homology:{exons_with_dup_level_above_thresh}'),as_text))
+        #     
+        #     dup_column_name<-glue('Exons with >10% with >{disclaimer_segdup_thresh*100}% homology')
+        #     disclaimer_table<-disclaimer_table%>%
+        #         rename('Add to disclaimer'=in_disclaimer,
+        #                !!sym(dup_column_name):=exons_with_dup_level_above_thresh,
+        #                'Gene name'=gene_symbol,
+        #                'Transcript name'=transcript_name)
+        #     output$disclaimer_table<-DT::renderDataTable(DT::datatable(disclaimer_table,
+        #                                              options=list(scrollX=F,pageLength=nrow(disclaimer_table),
+        #                                                           buttons = c('csv', 'excel'),
+        #                                                           dom = 'Bfrtip'),
+        #                                              extensions = 'Buttons',
+        #                                              selection='single',
+        #                                              rownames= FALSE,
+        #                                              filter = list(position = 'top', clear = FALSE)))
+        #         
+        #     output$disclaimer_text<-renderText(paste0(disclaimer_text%>%pull(as_text),collapse=' | '))
+        #     
+        #   })
+        # })
         
     })
     
